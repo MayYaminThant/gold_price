28e8ce7135d176b433e25154478eca16c2debee3
MayYaminThant <mayyamin17420@gmail.com>
2022-09-22 01:04:32 +0630

    remove s

--- lib/controller/gold_shop_controller.dart
+++ lib/controller/gold_shop_controller.dart
@@ -346,7 +346,7 @@ class GoldShopController with ChangeNotifier {
     FocusManager.instance.primaryFocus?.unfocus();
     Map<String, dynamic> insertData = Map<String, dynamic>.from(data);
     if (!isInsertedImage) {
-      insertData.remove('imageUrl');s
+      insertData.remove('imageUrl');
     }
 
     FirebaseFirestore.instance
